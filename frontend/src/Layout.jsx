9bb66539094904f6e857cd90e5b798577dcf5f49
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-06-26 16:47:14 +0200

    add Layout component

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./Components/NavBar";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+        <NavBar/>   
+        <main>
+            <Outlet/>
+        </main>
+
+    </>
+  )
+}
