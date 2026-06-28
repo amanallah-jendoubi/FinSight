981c726d5baafdc98b80a304907c146fd36128c3
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-06-29 00:19:15 +0200

    feat: add login logic

---
+++ backend/controllers/loginController.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+const ms = require('ms');
+const { createAccessAndRefreshJwts, findUserByEmail, saveRefreshToken, isValidRequest } = require('../services/authService');
+
+
+
+
+const login = async (req, res)=>{
+    const { isValid, errors } = isValidRequest(req, 'signup');
+    if (!isValid) {
+        return res.status(400).json({ errors });
+    }    
+    const user = findUserByEmail( req.body.email);
+    try {
+        if (user && await bcrypt.compare(req.body.password, user.passwordhash)) {
+            const { accessToken, refreshToken } = createAccessAndRefreshJwts(user);
+            await saveRefreshToken(user, refreshToken);
+            res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: ms('7d') });
+            return res.json({ accessToken }); // to store in memory 
+        }
+        return res.status(401).json({ 'message': 'please verify your credentials' });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
+};
+
+module.exports= login;
+
+
+
+
