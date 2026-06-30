9a7e734e73387bb64e96cf5bece43fcbbc9b3dac
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-06-30 22:22:26 +0200

    setting category endpoints

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -5,4 +5,5 @@ const router = express.Router ();
 router.post('/', require('../controllers/categoriesController').createCategory);
 router.get('/', require('../controllers/categoriesController').getAllCategories);
 
+
 module.exports = router ;
--- backend/services/categoriesService.js
+++ backend/services/categoriesService.js
@@ -2,7 +2,7 @@ const pool = require('../config/db');
 
 async function createCategory(categoryName) {
   const result = await pool.query(
-    'INSERT INTO "Category" (categoryName) VALUES ($1) RETURNING *',
+    'INSERT INTO "Category" (name) VALUES ($1) RETURNING *',
     [categoryName]
   );
   return result.rows[0];
