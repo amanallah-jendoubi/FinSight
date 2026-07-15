49b9d2442e3a2660863d57a152c7c3877c770889
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-07-15 09:13:49 +0200

    update budget api endpoint on the frontend

--- frontend/src/api/endpoints/budget.js
+++ frontend/src/api/endpoints/budget.js
@@ -1,3 +1,5 @@
 import api from '../axiosInstance';
 export const createBudget = (data) => api.post('/budget',data);
 export const getAllBudgets = () => api.get('/budget');
+export const updateBudget = (budgetId,updatedAmount) => api.patch(`/budget/${budgetId}`, updatedAmount);
+export const deleteBudget = (budgetId) => api.delete(`/budget/${budgetId}`);
