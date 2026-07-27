6b695d7a5e52fa843008dd3925064b26d0ce7f13
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-07-27 22:14:25 +0200

    add db credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',      
-  port: 5432,
-  user: 'postgres',
-  password: 'a123j',
-  database: 'finSight_DB',
+  host: process.env.POSTGRES_HOST,      
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 });
 
 module.exports = pool;
