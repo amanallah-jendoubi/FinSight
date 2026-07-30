127f3491294defcf6a36b3ee38c74fe4c4275256
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-07-30 16:15:04 +0200

    fixing production environment

--- backend/app.js
+++ backend/app.js
@@ -42,6 +42,7 @@ app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
+console.log(process.env.FRONTEND_URL);
 
 //built-in middleware for json requests
 app.use(express.json());
